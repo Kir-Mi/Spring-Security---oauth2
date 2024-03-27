d7aeb73cdee4220c0867b63ca81a511a39c30b86
kirill <you_can_more@mail.ru>
2024-03-27 11:09:48 +0100

    finish commit

--- src/main/java/com/example/SpringSecurityoauth2/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityoauth2/config/SecurityConfig.java
@@ -2,63 +2,32 @@ package com.example.SpringSecurityoauth2.config;
 
 import com.example.SpringSecurityoauth2.model.User;
 import com.example.SpringSecurityoauth2.repository.UserRepository;
-import com.example.SpringSecurityoauth2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequestEntityConverter;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 
-
-
-import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final UserRepository userRepository;
-    private final UserService userService;
 
     @Autowired
-    public SecurityConfig(UserService userService, UserRepository userRepository) {
-        this.userService = userService;
+    public SecurityConfig(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
-
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -77,65 +46,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-    /*@Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(gitHubClientRegistration());
-    }
-
-    @Bean
-    public ClientRegistration gitHubClientRegistration() {
-        return  ClientRegistration.withRegistrationId("github")
-                .clientId("ef369f98ed0dee708387")
-                .clientSecret("95e4147aed33626789e6fd6059d9bad952d50ae2")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("http://localhost:8080/login/oauth2/code/{registrationId}")
-                .scope("read:user")
-                .authorizationUri("https://github.com/login/oauth/authorize")
-                .tokenUri("https://github.com/login/oauth/access_token")
-                .userInfoUri("https://api.github.com/user")
-                .userNameAttributeName("id")
-                .clientName("GitHub")
-                .build();
-    }*/
-
-    /*@Bean
-    public PrincipalExtractor principalExtractor(UserRepository userDetailsRepo) {
-        return map -> {
-            String id = (String) map.get("sub");
-
-            User user = userDetailsRepo.findById(id).orElseGet(() -> {
-                User newUser = new User();
-
-                newUser.setId(id);
-                newUser.setName((String) map.get("name"));
-                newUser.setEmail((String) map.get("email"));
-
-                return newUser;
-            });
-
-
-            return userDetailsRepo.save(user);
-        };
-    }*/
-
-    /*@Bean
-    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
-        RestTemplate restTemplate = new RestTemplate();
-
-        return userRequest -> {
-            OAuth2AccessToken accessToken = userRequest.getAccessToken();
-            String userInfoUri = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri();
-
-            Map<String, Object> userAttributes = restTemplate
-                    .getForObject(userInfoUri, Map.class);
-
-            Set<GrantedAuthority> authorities = Collections.singleton(new OAuth2UserAuthority(userAttributes));
-            OAuth2User oauth2User = new DefaultOAuth2User(authorities, userAttributes, "sub");
-
-            return oauth2User;
-        };
-    }*/
 
     private GrantedAuthoritiesMapper userAuthoritiesMapper() {
         return authorities -> {
@@ -162,19 +72,4 @@ public class SecurityConfig {
             return grantedAuthorities;
         };
     }
-
-    /*@Bean
-    public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
-        OidcUserService oidcUserService = new OidcUserService();
-        return userRequest -> {
-            var oidcUser = oidcUserService.loadUser(userRequest);
-            var roles = oidcUser.getClaimAsStringList("spring_sec_roles");
-            var authorities = Stream.concat(oidcUser.getAuthorities().stream(), roles.stream()
-                            .filter(role -> role.startsWith("ROLE_"))
-                            .map(SimpleGrantedAuthority::new)
-                            .map(GrantedAuthority.class::cast))
-                    .toList();
-            return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
-        };
-    }*/
 }
